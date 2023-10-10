e55f51c0a8dc6bebd08207d5f11ee07cf09ea426
aicscloud <aicscloud@gmail.com>
2023-10-10 11:56:31 +0100

    update

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -13,7 +13,7 @@ import {
   Text,
   VStack
 } from '@chakra-ui/react';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { AiFillSetting } from 'react-icons/ai';
 import { BiSolidMessageRounded } from 'react-icons/bi';
 import { FaUser } from 'react-icons/fa';
@@ -24,6 +24,21 @@ import { RiAlertFill } from 'react-icons/ri';
 import './styles.css';
 
 function NavigationBar() {
+
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   function getDateAndTime() {
     const now = new Date();
 
@@ -111,44 +126,51 @@ function NavigationBar() {
   }
 
   return (
-    <Flex bg="#3A3C44" paddingY={2} paddingX={5} flexDirection={{ base: 'column', md: 'row' }} alignItems="center">
+    <Flex bg="#3A3C44" paddingY={5} paddingX={5} height='16' flexDirection={{ base: 'column', md: 'row' }} alignItems="center">
       <VStack justifyItems="center">
-        <Text className='textLogo'>GATESWAYDOC</Text>
+        <Text fontSize={windowWidth < 958 ? 16 : 20} className='textLogo'>GATESWAYDOC</Text>
         <Text className='dateText'>{getDateAndTime()}</Text>
       </VStack>
-      <InputGroup size="sm" mt={{ base: 4, md: 0 }} width={{ base: '100%', md: 250 }} ml={{ base: 0, md: 14 }}>
+      {windowWidth > 758 ? (
+      <InputGroup size="sm" mt={{ base: 0, md: 0 }} width={{ base: '100%', md: 250 }} ml={{ base: 0, md: 14 }}>
         <Input backgroundColor='whiteAlpha.800' rounded={50} placeholder='Rechercher un patient' _placeholder={{ color: 'blue.300' }} />
         <InputRightElement backgroundColor='blue.300' rounded={50}>
           <SearchIcon color='white' />
         </InputRightElement>
-      </InputGroup>
+      </InputGroup> ) : null}
       <HStack color='whiteAlpha.800' spacing={6} ml={{ base: 0, md: 14 }}>
-        <FaUser className='let' size={20} />
+        <FaUser display={{ base: 'none', md: 'block' }} className='let' size={20} />
         <MdMail size={24} />
         <AiFillSetting size={24} />
         <MdOutlinePersonalVideo size={24} />
       </HStack>
       <Spacer />
       <HStack spacing={6} color='whiteAlpha.800' mr={4}>
-        <Box className='boxLeftIcon'>
-          <BiSolidMessageRounded size={20} />
-        </Box>
-        <Box className='boxLeftIcon'>
-          <MdMail size={20} />
-        </Box>
-        <Box className='boxLeftIcon'>
-          <RiAlertFill size={20} />
-        </Box>
-        <Box backgroundColor="black" borderWidth={0} className='boxLeftIcon'>
-          <FaUser size={20} />
-        </Box>
+        {windowWidth > 958 ? (
+          <>
+            <Box className='boxLeftIcon'>
+              <BiSolidMessageRounded size={20} />
+            </Box>
+            <Box className='boxLeftIcon'>
+              <MdMail size={20} />
+            </Box>
+            <Box className='boxLeftIcon'>
+              <RiAlertFill size={20} />
+            </Box>
+            <Box backgroundColor="black" borderWidth={0} className='boxLeftIcon'>
+              <FaUser size={20} />
+            </Box>
+          </>) : null
+        }
+        
+        {windowWidth > 796 ? (
         <Menu isLazy>
           <MenuButton>
             <HStack>
               <Text>
                 GatewayDoc Admin
               </Text>
-              <IoMdArrowDropdown color='white'/>
+              <IoMdArrowDropdown color='white' />
             </HStack>
           </MenuButton>
           <MenuList color='blue.400'>
@@ -156,8 +178,9 @@ function NavigationBar() {
             <MenuItem>Create a Copy</MenuItem>
             <MenuItem>Mark as Draft</MenuItem>
           </MenuList>
-        </Menu>
-        <HiUserGroup size={26} />
+        </Menu>) : null}
+        {windowWidth > 1092 ? (
+          <HiUserGroup size={26} />) : null}
       </HStack>
     </Flex>
   );
